633d231fee707a8d9f453a7295b382689dacb9ef
zaniarahmad <130000994+zaniarahmad@users.noreply.github.com>
2026-08-14 17:35:39 -0400

    Fix robots indexing signals

--- app/robots.js
+++ app/robots.js
@@ -1,13 +1,11 @@
 import { siteContent } from '../lib/site-content';
 
 export default function robots() {
-  const isPreview = process.env.VERCEL_ENV === 'preview';
-
   return {
     rules: [
       {
         userAgent: '*',
-        ...(isPreview ? { disallow: '/' } : { allow: '/' }),
+        allow: '/',
       },
     ],
     sitemap: `${siteContent.siteUrl}/sitemap.xml`,
